818d77e46433dafd71cdd9ccc3b1c96e5d231773
park <dbswlqkr00@gmail.com>
2025-09-04 17:37:18 +0900

    feat: 질문 기능 구현
    - 질문 목록 및 상세 페이지 구현
    - 루트 URL 매핑 추가
    - QuestionService 클래스 생성
    - DataNotFoundException 예외 처리 추가

---
+++ sbb/src/main/java/com/myste/sbb/question/QuestionService.java
@@ -0,0 +1,31 @@
+package com.myste.sbb.question;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.myste.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+
+
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
